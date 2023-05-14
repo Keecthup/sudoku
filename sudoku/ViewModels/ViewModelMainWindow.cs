40026b9b56b3598f2e30b7ea4e92dacf5228087c
111 <111@DESKTOP-G2JMODD>
2023-05-14 22:58:45 +0300

    Добавьте файлы проекта.

---
+++ sudoku/ViewModels/ViewModelMainWindow.cs
@@ -0,0 +1,20 @@
+﻿using sudoku.ViewModels.basa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudoku.ViewModels
+{
+    internal class ViewModelMainWindow : ViewModel
+    {
+        private string _Title = "Судоку";
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set{ _title = value; }
+        }
+    }
+}

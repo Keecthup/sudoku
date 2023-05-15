940ae5f7c526f8102e9b9b71f4286d88f27eb7cc
Keecthup <118966408+Keecthup@users.noreply.github.com>
2023-05-15 14:44:14 +0300

    Icomand
    не работает(

---
+++ sudoku/sudoku/ViewModels/ViewModelMainWindow.cs
@@ -0,0 +1,40 @@
+﻿using sudoku.ViewModels.basa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace sudoku.ViewModels
+{
+    internal class ViewModelMainWindow : ViewModel
+    {
+#region Tittle
+        private string _Title = "Судоку";
+        public string Title
+        {
+            get { return _Title; }
+            set { _Title = value; }
+        }
+        #endregion
+        #region Команды
+        #region Close
+        
+        public ICommand CloseApplicationCommand { get;}
+
+        private void OnCloseApplicationCommandExecute(object sender, ExecutedRoutedEventArgs e) {
+        
+        }
+        private bool CanCloseApplicationCommandExecute(object sender, ExecutedRoutedEventArgs e) => true;
+        #endregion
+        #endregion
+        public ViewModelMainWindow()
+        {
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecute, CanCloseApplicationCommandExecute);
+
+        }
+    }
+
+    
+}
---
+++ sudoku/sudoku/infrostructure/commands/basovaya/Nachat_igruBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace sudoku.infrostructure.commands.basovaya
+{
+    internal abstract class Nachat_igruBase
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
